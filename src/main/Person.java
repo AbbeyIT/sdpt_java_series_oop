ae6fca5a6943945d8857a9e900ead1b2ac379c34
Abbey <abbeysantos1210@gmail.com>
2025-06-02 20:25:06 +0800

    refactor: encapsulate Person attributes with constructor

--- src/main/Person.java
+++ src/main/Person.java
@@ -6,5 +6,13 @@ public class Person {
 	String lastName;
 	char sex;
 	int age;
+	
+	Person(String firstName, String lastName, char sex, int age){
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.sex = sex;
+		this.age = age;
+	}
 
 }
