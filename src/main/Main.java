06804c37143456e89370889ebce982e47c396d4e
Abbey <abbeysantos1210@gmail.com>
2025-06-02 20:23:34 +0800

    refactor: use constructor for object initialization

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,15 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Person p = new Person();
 		
-		p.firstName = "Alex";
-		p.lastName = "Albon";
-		p.sex = 'M';
-		p.age = 29;
+		// Person.java
+		Person p = new Person("Alex", "Albon", 'M', 29); //Constructors
+		
+//		p.firstName = "Alex";
+//		p.lastName = "Albon";
+//		p.sex = 'M';
+//		p.age = 29;
 		
 		System.out.println(p.firstName);
 		System.out.println(p.age);
+		
+		
+		// Product.java
+		Product p1 = new Product("Albono",  4.6f);
+		System.out.println(p1.price);
 	}
 
 }
