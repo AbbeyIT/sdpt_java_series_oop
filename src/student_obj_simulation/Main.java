9d4a335c13f3e17aedf0e6f217cbb5d0d2cfffd2
Abbey <abbeysantos1210@gmail.com>
2025-06-04 12:12:14 +0800

    chore: add main file for student obj simulation

---
+++ src/student_obj_simulation/Main.java
@@ -0,0 +1,16 @@
+package student_obj_simulation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//STUDENT Object Simulation
+		
+		Student s1 = new Student("Lance", "Stroll", 2025, "Engineering", "AM25", 86.4f, 99f);
+		
+		s1.introduceSelf();
+		s1.evaluateGrade();
+
+	}
+
+}
