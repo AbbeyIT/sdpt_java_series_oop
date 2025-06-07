0243279e9b4b8344c99fde4578bb1b86b631e786
Abbey <abbeysantos1210@gmail.com>
2025-06-07 19:29:35 +0800

    chore: add weak enemy class for polymorphism

---
+++ src/polymorphism/WeakEnemy.java
@@ -0,0 +1,14 @@
+package polymorphism;
+
+public class WeakEnemy extends Enemy{
+	
+	WeakEnemy() {
+		name = "Weak Enemy";
+		hp = 10;
+	}
+	
+	void dialog() {
+		System.out.println(name + ": I am weak.");
+	}
+
+}
