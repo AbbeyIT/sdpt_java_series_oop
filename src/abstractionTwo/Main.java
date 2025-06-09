b14a93f1aa0c477ad5c285176b84b8112e198738
Abbey <abbeysantos1210@gmail.com>
2025-06-09 19:32:01 +0800

    chore: add main for abstract interface

---
+++ src/abstractionTwo/Main.java
@@ -0,0 +1,18 @@
+package abstractionTwo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Dog d = new Dog();
+		Bird b = new Bird();
+		Frog f = new Frog();
+		
+		d.makeSound();
+		b.makeSound();
+		f.makeSound();
+		
+		d.walk();
+	}
+
+}
