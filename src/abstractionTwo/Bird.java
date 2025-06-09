7c73789cf7e0a5a657563c6fb356e28641ab3f3f
Abbey <abbeysantos1210@gmail.com>
2025-06-09 19:33:00 +0800

    chore: add bird dog frog animal  for abstract interface

---
+++ src/abstractionTwo/Bird.java
@@ -0,0 +1,17 @@
+package abstractionTwo;
+
+public class Bird implements Animal, AirAnimal, LandAnimal {
+	
+	public void makeSound() {
+		System.out.println("Chirp");
+	}
+	
+	public void fly() {
+		System.out.println("Flying");
+	}
+	
+	public void walk() {
+		System.out.println("Walking");
+	}
+
+}
---
+++ src/abstractionTwo/Frog.java
@@ -0,0 +1,17 @@
+package abstractionTwo;
+
+public class Frog implements Animal, LandAnimal, WaterAnimal{
+	
+	public void makeSound() {
+		System.out.println("Ribbit");
+	}
+	
+	public void walk() {
+		System.out.println("Walking");
+	}
+
+	public void swim() {
+		System.out.println("Swimming");
+	}
+
+}
